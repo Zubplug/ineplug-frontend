851b9bf8d45a32035eaab22f36f61f32a4f576f4
Inemesit Akpan <zubplug@gmail.com>
2025-04-23 02:53:49 +0100

    Fix baseURL to use VITE_API_URL instead of localhost

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,12 +9,14 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token =
-  localStorage.getItem("adminToken") ||
-  localStorage.getItem("token") ||
-  sessionStorage.getItem("token");
+    localStorage.getItem("adminToken") ||
+    localStorage.getItem("token") ||
+    sessionStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
